26b48663b0463ecd4d7c4abdc137dd299d7ba482
Hugo <hhugo.avila97@gmail.com>
2023-10-14 11:33:41 -0300

    feat: ✨cachear archivos

--- sw.js
+++ sw.js
@@ -1,9 +1,28 @@
+const nombreCache = 'apv-v1';
+
+const archivos = [
+    '/',
+    '/index.html',
+    '/css/bootstrap.css',
+    '/css/styles.css',
+    '/js/app.js',
+    '/js/apv.js'
+];
+
 
 //Cuando se instala el Service Worker
 self.addEventListener('install', e => {
     console.log('Instalado el Service Worker');
 
-    console.log(e)
+  e.waitUntil(
+    caches.open(nombreCache)
+        .then( cache => {
+            console.log('Cacheando');
+            cache.addAll(archivos);
+        })
+  )  
+
+
 })
 
 //Activar el service worker
@@ -16,5 +35,5 @@ self.addEventListener('activate', e => {
 //Evento fetch para descargar archivos estaticos
 self.addEventListener('fetch', e => {
     console.log('Fetch...', e);
-    
+
 })
